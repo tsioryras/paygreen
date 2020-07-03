ffc828d749feb862b7229e4434dbf079aad48563
Tsioryras <tsioryrasamoely@gmail.com>
2020-07-03 20:25:10 +0200

    all done

---
+++ PaygreenIntegration/README.md
@@ -0,0 +1,16 @@
+# PAYGREEN TEST
+
+PAYGREEN TEST est un projet Symfony permettant d'effectuer des transactions et d'en récupérer les informations 
+
+---
+## Installation
+
+* Lancer les commandes suivantes dans le repertoire où vous souhaitez mettre le projet : 
+* `git clone https://username@bitbucket.org/media-sante/omega.git` (replace `username` by your bitbucket username)
+* `cd PaygreenIntegration`
+* `composer install`
+* `yarn install`
+* `symfony serve --no-tls --daemon`
+* taper dans la barre d'addresse de votre navigateur internet -- http://127.0.0.1:8000 -- (port par défaut)
+
+---
--- PaygreenIntegration/assets/js/transaction.js
+++ PaygreenIntegration/assets/js/transaction.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
             type: 'post',
             dataType: 'json',
             data: {
+                'companyName': $('#companyName').val(),
                 'firstName': $('#firstName').val(),
                 'lastName': $('#lastName').val(),
                 'email': $('#email').val(),
@@ -43,10 +44,38 @@ $(document).ready(function () {
                 'pid': $('#pid').val()
             },
             success: function (data) {
-                console.log(data);
+
                 if (data.success) {
-                    let result = [];
-                    
+                    let createdAt = new Date(data.data.createdAt);
+                    let valuedAt = new Date(data.data.valueAt);
+                    let result = '<tbody>' +
+                        '<tr>' +
+                        '<th scope="row">ID</th>' +
+                        '<td>' + data.data.id + '</td>' +
+                        '</tr>' +
+                        '<tr>' +
+                        '<th scope="row">AMOUNT</th>' +
+                        '<td>' + data.data.amount / 100 + ' ' + data.data.currency + ' </td>' +
+                        '</tr>' +
+                        '<tr><th scope="row">TYPE</th>' +
+                        '<td>' + data.data.type + ' </td>' +
+                        '</tr>' +
+                        '<tr><th scope="row">STATUS</th>' +
+                        '<td>' + data.data.result.status + ' </td>' +
+                        '</tr>' +
+                        '<tr><th scope="row">BUYER</th>' +
+                        '<td>' + data.data.buyer.companyName + ' (' + data.data.buyer.id + ' ' + data.data.buyer.lastName + ')</td>' +
+                        '</tr>' +
+                        '<tr><th scope="row">CREATED AT</th>' +
+                        '<td>' + createdAt.toLocaleDateString() + ' </td>' +
+                        '</tr>' +
+                        '<tr><th scope="row">VALUE AT</th>' +
+                        '<td>' + valuedAt.toLocaleDateString() + ' </td>' +
+                        '</tr>' +
+                        '<tbody>';
+                    $('table').html(result);
+                    $('#pid').val('');
+
                 } else {
                     $('#pid').addClass('is-invalid');
                 }
